7c6ad629f23e8865685a9441d14a165f743946d6
Samuel <samuelsouza0802@gmail.com>
2025-09-17 05:00:43 -0300

    Pagina com barra de Usuário

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<x-layouts.main-layout pageTitle="Home">
+
+
+
+    <div class="container-fluid mt-5">
+       <div class="row">
+
+         <div class="col p-3">
+             <p class="text-center display-6">Bem vindo a página Home</p>
+         </div>
+
+       </div>
+    </div>
+
+</x-layouts.main-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 //Rotas para usuário não autenticado (VISITANTE)
@@ -18,10 +19,9 @@ Route::middleware(['guest'])->group(function(){
      Route::get('/new_user_confirmation/{token}',[AuthController::class, 'new_user_confirmation'])->name('new_user_confirmation');
 });
 
+//Rotas para usuário autenticado (AUTENTICADO)
 Route::middleware(['auth'])->group(function(){
-    Route::get('/', function(){
-             echo 'Home';
-    })->name('home');
 
+   Route::get('/', [MainController::class, 'home'])->name('home');
    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
